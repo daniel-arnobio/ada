766095b60bd57b980815f6e82253a8bf7b7374f9
Daniel Arnóbio <daniel.arnobio1@gmail.com>
2022-10-25 17:58:47 -0300

    proj final

--- projeto buscador tweets/proj/proj_buscador2.py
+++ projeto buscador tweets/proj/proj_buscador2.py
@@ -1,18 +1,12 @@
 
 import json, csv
 
-import os
-cwd = os.getcwd()
-files = os.listdir(cwd)
-
-
 ### Lendo o arquivo csv
-arquivo = open(files[1], 'r', encoding='utf-8')
+arquivo = open('C:\projeto_LP_tweets_2022.csv', 'r',  encoding="utf-8")
 planilha = csv.reader(
     arquivo, 
     delimiter = ',', 
     lineterminator = '\n')
-
 
 lista_tweets = list(planilha)
 arquivo.close()
@@ -25,19 +19,19 @@ def buscar_data(data):
         print('data | conteúdo | assunto')
         ## um for para varrer todas as datas da lista_tweets e comparar com a data inputada
         for linha in range(len(lista_tweets)):
-            data2 = lista_tweets[linha][0][0:10]
+                data2 = lista_tweets[linha][0][0:10]
                         #dia                    #mes                       #ano
-            if data[0:2]==data2[8:11] and data[3:5]==data2[5:7] and data[6:12]==data2[0:4]:
+                if data[0:2]==data2[8:11] and data[3:5]==data2[5:7] and data[6:12]==data2[0:4]:
                         #data           #conteudo                #assunto
-                    datas.append(lista_tweets[linha][0])
-                    conteudo.append(lista_tweets[linha][3])
-                    assunto_.append(lista_tweets[linha][4])
-                    print(f'{lista_tweets[linha][0][0:10][8:11]}/{lista_tweets[linha][0][0:10][5:7]}/{lista_tweets[linha][0][0:10][0:4]} | {lista_tweets[linha][3]} | {lista_tweets[linha][4]}')
+                        datas.append(lista_tweets[linha][0])
+                        conteudo.append(lista_tweets[linha][3])
+                        assunto_.append(lista_tweets[linha][4])
+                        print(f'{lista_tweets[linha][0][0:10][8:11]}/{lista_tweets[linha][0][0:10][5:7]}/{lista_tweets[linha][0][0:10][0:4]} | {lista_tweets[linha][3]} | {lista_tweets[linha][4]}')
         dic = {'data': datas, 'conteudo': conteudo, 'assunto': assunto_}
         return dic
 
 
-#funcao para buscar tweets por termo
+#funcao para buscar termo no tweet
 def buscar_termo(termo):
     datas = []
     conteudo = []
@@ -53,7 +47,6 @@ def buscar_termo(termo):
     dic = {'data': datas, 'conteudo': conteudo, 'assunto': assunto_}
     return dic
 
-#funcao para buscar tweets por assunto
 def buscar_assunto(assunto):
     print('data | conteúdo | assunto')
     datas = []
@@ -69,13 +62,13 @@ def buscar_assunto(assunto):
     dic = {'data': datas, 'conteudo': conteudo, 'assunto': assunto_}
     return dic
 
-#funcao que salva busca em arquivo json
+#funcao que salva em arquivo json
 def salvar_busca(dic):
     json_object = json.dumps(dic) 
-    with open(r"C:\Users\Jorge Lira\Downloads\colab\Aula 2\projeto\busca.json", "w") as outfile: 
+    with open("busca.json", "w") as outfile: 
         outfile.write(json_object)
 
-#funcao menu de opcoes
+
 def menu():
     print('Boas vindas ao nosso sistema:')
     print('1 - Buscar tweets por data')
@@ -108,3 +101,22 @@ while option != 5:
     option = int(input('Escreva sua opção: '))
 
 print('Obrigado por usar nosso programa. Até a próxima!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
